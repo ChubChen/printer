0cb0ca795a826803962ab40149f1b072dabc01c3
ChubChen <CH@localhost.localdomain>
2015-04-08 16:43:40 +0800

    update code

--- saomiaokou.js
+++ saomiaokou.js
@@ -19,6 +19,8 @@ var saomiao = function(){
     self.winRow = 1;
     self.scoreConf = [{"10":1,"42":2,"24":4,"01":8},{"20":1,"50":2,"05":4,"02":8},{"21":1,"51":2,"15":4,"12":8},{"30":1,"52":2,"25":4,"03":8},{"31":1,"90":2,"09":4,"13":8},{"32":1,"00":2,"33":4,"23":8},{"40":1,"11":2,"99":4,"04":8},{"41":1,"22":2,"14":8}];
     self.scoreRow = 8;
+    self.shunXuArray = [1,0,0,3];
+
 
 
 };
@@ -78,7 +80,8 @@ saomiao.prototype.start = function(){
 };
 //
 saomiao.prototype.trans = function(confObj, row, array){
-   var resultArray = new Array();
+    var self = this;
+    var resultArray = new Array();
    resultArray.push("00 00 ");
    for(var i = 0; i < row ; i++){
        var bool = true;
@@ -90,8 +93,8 @@ saomiao.prototype.trans = function(confObj, row, array){
                continue;
            }
            var result = 0;
-           if(array[j]){
-               var tempArray = array[j].split(',');
+           if(array[self.shunXuArray[j]]){
+               var tempArray = array[self.shunXuArray[j]].split(',');
                for(var k = 0 ;k < tempArray.length; k++){
                    if(confObj[i][tempArray[k]]){
                        result += confObj[i][tempArray[k]];
@@ -102,11 +105,12 @@ saomiao.prototype.trans = function(confObj, row, array){
        }
        resultArray.push(" ");
    }
-   //console.log(resultArray.join(""));
+   console.log(resultArray.join(""));
    return resultArray.join("");
 }
 
 saomiao.prototype.changciTrans = function(match){
+    var self = this;
     var Obj = {"0":"0","1":"1","2":"2",3:"3","4":"4","5":"8","6":"6","7":"a","8":"b","9":"c"};
     var weekArray = new Array();
     for(var i = 0; i < 3 ; i++){
@@ -119,8 +123,8 @@ saomiao.prototype.changciTrans = function(match){
                 continue;
             }
             var tempArray = [0,0,0];
-            if(match[j]){
-                 tempArray = match[j].toString().split("").reverse();
+            if(match[self.shunXuArray[j]]){
+                 tempArray = match[self.shunXuArray[j]].toString().split("").reverse();
             }
             weekArray.push(Obj[tempArray[i]]);
         }
@@ -131,38 +135,55 @@ saomiao.prototype.changciTrans = function(match){
 };
 
 saomiao.prototype.btypeTrans =  function(bType, multiple){
+    var self = this;
     var resultArray = new Array();
-    var multiplePeizhi = {"1":"1","2":"2","3":"4","4":"8","5":"6","6":"7","7":"b","8":"c","9":"e","10":"f"};
-    var tenMultiple= {"1":"2","2":"4","3":"8","4":"a","5":"b"};
+    var betTypeConf = [[{"21":"1","31":"2","33":"4","34":"8","41":"1","44":"2","45":"4","46":"8","71":"1","77":"2","78":"4","721":"8"},{"735":"1","7120":"2","411":"1","11":"8"}],[{"51":"1","55":"2","56":"4","510":"8","81":"1","88":"2","89":"4","828":"8"},{"516":"1","520":"2","526":"4","856":"1","870":"2","8247":"4"}],[{"61":"1","66":"2","67":"4","615":"8"},{"620":"1","622":"2","635":"4","642":"8"}],[{"650":"1","657":"2"},{}]];
+    var multiplePeizhi = {"0":"0","1":"1","2":"2","3":"4","4":"8","5":"6","6":"7","7":"b","8":"c","9":"e","10":"f"};
+    var tenMultiple= {"0":"0","1":"2","2":"4","3":"8","4":"a","5":"b","6":"d"};
     resultArray.push("00 00 ");
-    if(bType == "21"){
-        resultArray.push("01");
-    }else if(bType == "31"){
-        resultArray.push("02");
-    }else if(bType == "33"){
-        resultArray.push("04");
-    }else if(bType == "34"){
-        resultArray.push("08");
-    }else if(bType == "11"){
-        resultArray.push("80");
-    }
-    if(multiple <= 10){
-       resultArray.push(" 0"+multiplePeizhi[multiple]);
-       resultArray.push(" 00 00 00 00 00 00 00 00");
-    }else if(multiple < 60){
-       resultArray.push(" 0"+multiplePeizhi[multiple%10]);
-       resultArray.push(" 00 00 00");
-       resultArray.push(" 0"+tenMultiple[Math.floor(multiple/10)]);
-       resultArray.push(" 00 00");
-    }else{
-        resultArray.push(" 0"+multiplePeizhi[multiple%10]);
-        resultArray.push(" 00 00 00");
-        resultArray.push(" 0"+tenMultiple[Math.floor(multiple/10)-5]);
-        resultArray.push(" 00 02");
+    for(var i = 0 ;i< 4; i++){
+        var bool = true;
+        for(var j = 0; j< 3; j++) {
+            if (j == 2 && bool) {
+                bool = false;
+                j--;
+                resultArray.push(" 0");
+                continue;
+            }
+            if(j < 2 ){
+                if(betTypeConf[i][self.shunXuArray[j]][bType]){
+                    resultArray.push(betTypeConf[i][self.shunXuArray[j]][bType]);
+                }else{
+                    resultArray.push("0")
+                }
+            }else{
+                if(i == 0){
+                    resultArray.push(multiplePeizhi[multiple%10]);
+                }else if (i == 1){
+                    resultArray.push("0");
+                }else if (i == 2){
+                    if(multiple <= 60){
+                        resultArray.push(tenMultiple[Math.floor(multiple/10)]);
+                    }else if(multiple > 60){
+                        resultArray.push(tenMultiple[Math.floor(multiple/10)-5]);
+                    }else{
+                        resultArray.push("0");
+                    }
+                }else{
+                    if(multiple > 60){
+                        resultArray.push("2");
+                    }else{
+                        resultArray.push("0");
+                    }
+                }
+            }
+        }
+        resultArray.push(" ");
     }
     console.log(resultArray.join(""));
     return resultArray.join("");
 };
 
 var test = new saomiao();
-test.start();
+//test.start();
+test.trans(test.winConf, test.winRow, ["1,3","1","0,1"]);
